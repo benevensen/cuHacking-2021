9f730a7e996dfc82502169ca138e3fa6217fc603
bennibloo <57432484+bennibloo@users.noreply.github.com>
2021-01-30 15:53:43 -0500

    js script

---
+++ frontend/organiser.js
@@ -0,0 +1,10 @@
+function handleButtonClicks() {
+    $( ".category-button" ).click(function() {
+        $(this ).css('background', 'tomato');
+        $(this).siblings().css('background', 'rgba(19, 19, 19, 0.76)')
+    });
+}
+
+$(function() {
+    handleButtonClicks();
+});
