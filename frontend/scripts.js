9f3fa0026ff0a81bdd74d04226a27077924a4edf
Daniel Innes <Daniel.W.Innes@gmail.com>
2021-01-31 09:53:28 -0500

    getAuth

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,5 +1,4 @@
 let ctr = 0;
-
 function remove(el) {
     let element = el;
     element.classList.add("hidden");
@@ -7,5 +6,21 @@ function remove(el) {
 }
 
 function checkAnswer(el) {
-    
+
+}
+
+function getAuth(entry_code) {
+    fetch("http://localhost:8081/register", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            "event_id": entry_code
+        },
+    })
+        .then(res => res.json())
+        .then((data) => {
+            localStorage.setItem('auth_code', data['token'])
+        })
 }
